01b87ff69955203cd63df2ef5b4271f03a4b6149
cdelllies <cdelllies@github.com>
2020-01-22 13:12:32 +0100

    some edits

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var app = require('http').createServer(handler),
 io = require('socket.io').listen(app),
-ws = require("websocket-server"),
 fs = require("fs"),
 url = require("url"),
-port = process.env.PORT || 5000,
+port = process.env.PORT || 5001,
 queue = {
 	'W' : [],
 	'B' : [],
