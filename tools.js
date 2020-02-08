57857add2b84c88eae6d67328189b4d4680d1b6e
Alienovore <45195758+Alienovore@users.noreply.github.com>
2020-02-08 16:14:21 +0100

    refactor(all): rm useless function

--- tools.js
+++ tools.js
@@ -8,13 +8,3 @@ function findKey(array, key) {
   }
   return found;
 }
-
-function findIndex(array, key) {
-  let i = 0;
-  for (i = 0; i < array.length; i++) {
-    if (array[i].collision == key) {
-      break;
-    }
-  }
-  return i;
-}
